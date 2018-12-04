8f8f4b9305dd10e6fd1225c1a3324d94d2851aa1
Christian Vasquez <christianvasquezmelo@gmail.com>
2018-12-04 11:38:52 -0400

    Rearrange code

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Pellys.Models;
 
@@ -9,25 +6,6 @@ namespace Pellys.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
-
-   
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-
-            modelBuilder.Entity<ActorMovies>().HasKey(Am => new { Am.ActorId, Am.MovieId });
-            modelBuilder.Entity<PartnerActors>().HasKey(Pa => new { Pa.PartnerId, Pa.ActorId});
-            modelBuilder.Entity<PartnerDirector>().HasKey(Pd => new { Pd.PartnerId, Pd.DirectorId });
-            modelBuilder.Entity<PartnerGenre>().HasKey(Pg => new { Pg.PartnerId, Pg.GenreId });
-            modelBuilder.Entity<PartnerWaitingList>().HasKey(Pw => new { Pw.PartnerId, Pw.WaitingListId });
-        }
-
-
         public virtual DbSet<Actor> Actors { get; set; }
         public virtual DbSet<Director> Directors { get; set; }
         public virtual DbSet<Form> Forms { get; set; }
@@ -37,5 +15,15 @@ namespace Pellys.Data
         public virtual DbSet<Tape> Tapes { get; set; }
         public virtual DbSet<WaitingList> WaitingLists { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ActorMovies>().HasKey(Am => new {Am.ActorId, Am.MovieId});
+            modelBuilder.Entity<PartnerActors>().HasKey(Pa => new {Pa.PartnerId, Pa.ActorId});
+            modelBuilder.Entity<PartnerDirector>().HasKey(Pd => new {Pd.PartnerId, Pd.DirectorId});
+            modelBuilder.Entity<PartnerGenre>().HasKey(Pg => new {Pg.PartnerId, Pg.GenreId});
+            modelBuilder.Entity<PartnerWaitingList>().HasKey(Pw => new {Pw.PartnerId, Pw.WaitingListId});
+        }
     }
 }
